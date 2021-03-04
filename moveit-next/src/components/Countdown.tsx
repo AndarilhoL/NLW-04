e57d88a200bb7389ca82486fbd7869d87260dfb7
AndarilhoL <leonardosantos2742@gmail.com>
2021-03-04 14:08:52 -0300

    finalizando projeto

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,10 +1,9 @@
-import { useState, useEffect, useContext } from 'react';
-import { ChallengesContext } from '../contexts/challengesContext';
+import { useContext } from 'react';
 import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
-    const {minutes, seconds, hasFinished, isActive, startCountdown ,resetCountdown} = useContext(CountdownContext)
+    const { minutes, seconds, hasFinished, isActive, startCountdown, resetCountdown } = useContext(CountdownContext)
 
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
@@ -14,7 +13,7 @@ export function Countdown() {
             <div className={styles.countdownContainer}>
                 <div>
                     <span>{minuteLeft}</span>
-                    <span>{minuteRight}</span> 
+                    <span>{minuteRight}</span>
                 </div>
                 <span>:</span>
                 <div>
